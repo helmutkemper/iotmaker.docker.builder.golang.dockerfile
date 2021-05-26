bc732a52cdf128ba697539e841a33351db054623
Helmut Kemper <helmut.kemper@gmail.com>
2021-05-26 13:42:28 -0300

    initial commit

--- typeDockerfileGolang.go
+++ typeDockerfileGolang.go
@@ -4,6 +4,7 @@ import (
 	"github.com/docker/docker/api/types/mount"
 	iotmakerdocker "github.com/helmutkemper/iotmaker.docker/v1.0.1"
 	"io/fs"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -20,6 +21,20 @@ type ChangePort struct {
 }
 
 type DockerfileGolang struct{}
+
+// Prayer
+//
+// English: Programmer prayer
+//
+// Português: Oração do programador
+func (e *DockerfileGolang) Prayer() {
+	log.Print("Português:")
+	log.Print("Código nosso que estais em Golang\nSantificado seja Vós, Console\nVenha a nós a Vossa Reflexão\nE seja feita a {Vossa chave}\nAssim no if(){}\nComo no else{}\nO for (nosso; de cada dia; nos dai hoje++)\nDebugai as nossas sentenças\nAssim como nós colocamos\nO ponto e vírgula esquecido;\nE não nos\n\tdeixe errar\n\t\tindentação\nMas, livreiros das funções recursivas\nA main ()")
+	log.Print("")
+	log.Print("English:")
+	log.Print("Our program, who art in memory,\ncalled by thy name;\nthy operating system run;\nthy function be done at runtime\nas it was on development.\nGive us this day our daily output.\nAnd forgive us our code duplication,\nas we forgive those who\nduplicate code against us.\nAnd lead us not into frustration;\nbut deliver us from GOTOs.\nFor thine is algorithm,\nthe computation, and the solution,\nlooping forever and ever.\nReturn;")
+	log.Print("")
+}
 
 func (e *DockerfileGolang) MountDefaultDockerfile(args map[string]*string, changePorts []ChangePort, openPorts []string, volumes []mount.Mount) (dockerfile string, err error) {
 
